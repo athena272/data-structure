3eacf9fef225936d3c3f9256aa2ba0482ca943aa
athena272 <guilhermera272@gmail.com>
2023-09-07 08:40:08 -0300

    Update index.c

--- 02-pointers/code-1/index.c
+++ 02-pointers/code-1/index.c
@@ -10,5 +10,15 @@ int main()
     value1 = &value2;
     char1 = &char2;
 
-    printf("Valor da variavel value2: %d\n", value2);
+    printf("Variable value value2: %d\n", value2);
+    printf("Memory address value2: %p\n", &value2);
+    printf("Memory address value1: %p\n", value1);
+    printf("Variable value value1: %d\n", *value1);
+
+    printf("==========================================\n");
+
+    printf("Variable value char2: %c\n", char2);
+    printf("Memory address char2: %p\n", &char2);
+    printf("Memory address char1: %p\n", char1);
+    printf("Variable value char1: %c\n", *char1);
 }
