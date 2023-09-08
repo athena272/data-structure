4377667e19e9a448bf9ad1c96ee5f46612a24505
athena272 <guilhermera272@gmail.com>
2023-09-08 04:33:25 -0300

    Update index.c

--- 02-pointers/code-2/index.c
+++ 02-pointers/code-2/index.c
@@ -2,11 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-void printVector(int *vet)
+void printVector(int *vet, int size)
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < size; i++)
     {
-        printf("%d ", *(vet + i));
+        printf("%2d ", *(vet + i));
+    }
+
+    printf("\n");
+}
+
+void printAlfa(char *vet, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%2c ", *(vet + i));
+    }
+
+    printf("\n");
+}
+
+void createRandomVector(int *vet, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        *(vet + i) = rand() % 100;
+    }
+}
+
+void createVector(int *vet, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("Digite o %dº valor: ", i + 1);
+        scanf("%d", (vet + i));
     }
 
     printf("\n");
@@ -15,14 +44,20 @@ void printVector(int *vet)
 int main()
 {
     srand(time(NULL));
+
     int vet[10];
+    int vet2[5];
+    char alfa[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 
-    for (int i = 0; i < 10; i++)
-    {
-        vet[i] = rand() % 100;
-    }
+    createRandomVector(vet, 10);
 
-    printVector(vet);
+    printVector(vet, 10);
+
+    printAlfa(alfa, 10);
+
+    createVector(vet2, 5);
+
+    printVector(vet2, 5);
 
     // printf("%p\t%p\n", vet, &vet[0]);
 }
