1c6d08972d13635fecac265b321e296f0619656e
athena272 <guilhermera272@gmail.com>
2024-03-03 23:33:53 -0300

    Update index.c

--- C/00-vector/code-07/index.c
+++ C/00-vector/code-07/index.c
@@ -1,19 +1,50 @@
-//Gera numeros aleatorios sem repeticao
+// Gera numeros aleatorios sem repeticao
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    int i, vet[25];
+// Função de comparação para qsort
+int comparar(const void *a, const void *b)
+{
+    // Convertendo os ponteiros genéricos de volta aos tipos originais
+    int *valorA = (int *)a;
+    int *valorB = (int *)b;
+
+    // Compara os valores apontados por a e b
+    return *valorA - *valorB;
+}
+
+int main()
+{
+    int i = 0, isEqual, vet[100];
     srand(time(NULL));
 
-    for (int i = 0; i < 20; i++)
+    do
     {
-        vet[i] = rand() % 100;
-    }
 
-    for (int i = 0; i < 20; i++)
+        vet[i] = rand() % 100;
+        isEqual = 0;
+        for (int j = 0; j < i; j++)
+        {
+            // Se o elemento já exister, troque por outro
+            if (vet[j] == vet[i])
+            {
+                isEqual = 1;
+            }
+        }
+
+        if (isEqual == 0)
+        {
+            i++;
+        }
+
+    } while (i < 100);
+
+    // Classificar o array usando qsort
+    qsort(vet, 100, sizeof(int), comparar);
+
+    for (int i = 0; i < 100; i++)
     {
         printf("%d ", vet[i]);
     }
