78eef528281aee33a5fc5e01df25243f3e3cc023
athena272 <guilhermera272@gmail.com>
2024-03-05 00:16:21 -0300

    Update index.c

--- C/03-struct/code-01/index.c
+++ C/03-struct/code-01/index.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 //criar vetor struct
 
 typedef struct {
@@ -16,4 +18,27 @@ void printAPerson(Person person) {
     printf("\tAge: %d\n", person.age);
     printf("\tSex: %s\n", person.sex);
     printf("\tBirth Date: %d/%d/%d\n", person.birthDate.day, person.birthDate.month, person.birthDate.year);
+}
+
+Person readPerson() {
+    Person person;
+    printf("Type your name: ");
+    fgets(person.name, 100, stdin);
+    printf("Type your age: ");
+    scanf("%d", &person.age);
+    scanf("%c");
+    printf("Typer M or F to define your sex: ");
+    scanf("%c", &person.sex);
+    printf("Type your birth date on the format dd mm yyyy: ");
+    scanf("%d%d%d", &person.birthDate.day, &person.birthDate.month, &person.birthDate.year);
+
+    return person;
+}
+
+int main() {
+    Person person;
+    person =  readPerson();
+    printAPerson(person);
+
+    return 0;
 }
