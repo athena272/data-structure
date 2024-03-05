bb03f1dc24b2ae1f89bed29fed69289852668fbf
athena272 <guilhermera272@gmail.com>
2024-03-04 23:56:38 -0300

    Update index.c

--- C/03-struct/code-01/index.c
+++ C/03-struct/code-01/index.c
@@ -10,3 +10,10 @@ typedef struct {
     char sex;
     char name[100];
 } Person;
+
+void printAPerson(Person person) {
+    printf("\tName: %s\n", person.name);
+    printf("\tAge: %d\n", person.age);
+    printf("\tSex: %s\n", person.sex);
+    printf("\tBirth Date: %d/%d/%d\n", person.birthDate.day, person.birthDate.month, person.birthDate.year);
+}
