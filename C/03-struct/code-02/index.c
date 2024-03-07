005cbc9f5b1f5e602d429493d0db07592f126adf
athena272 <guilhermera272@gmail.com>
2024-03-06 23:33:54 -0300

    Update index.c

--- C/03-struct/code-02/index.c
+++ C/03-struct/code-02/index.c
@@ -2,13 +2,39 @@
 #include <stdlib.h>
 // criar uma matriz struct
 
-typedef struct {
+typedef struct
+{
     int day, month, year;
 } BirthDate;
 
-typedef struct {
+typedef struct
+{
     BirthDate birthDate;
     int age;
     char sex;
     char name[100];
 } Person;
+
+void printAPerson(Person person)
+{
+    printf("\tName: %s\n", person.name);
+    printf("\tAge: %d\n", person.age);
+    printf("\tSex: %c\n", person.sex);
+    printf("\tBirth Date: %d/%d/%d\n", person.birthDate.day, person.birthDate.month, person.birthDate.year);
+}
+
+Person readPerson()
+{
+    Person person;
+    printf("Type your name: ");
+    fgets(person.name, 100, stdin);
+    printf("Type your age: ");
+    scanf("%d", &person.age);
+    scanf("%c");
+    printf("Typer M or F to define your sex: ");
+    scanf("%c", &person.sex);
+    printf("Type your birth date on the format dd mm yyyy: ");
+    scanf("%d%d%d", &person.birthDate.day, &person.birthDate.month, &person.birthDate.year);
+
+    return person;
+}
