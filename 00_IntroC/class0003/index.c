f0d5cc15c210d8df0bfbfc0c36a211577d8183b7
athena272 <guilhermera272@gmail.com>
2024-05-21 21:20:45 -0300

    Create index.c

---
+++ 00_IntroC/class0003/index.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Pressione qualquer tecla para continuar");
+
+    // ler um caracter do teclado
+    scanf("%c");
+    // ou
+    getchar();
+
+    return 0;
+}
