1346bd67213c08779aa976f4e53f6147607a4e9b
athena272 <guilhermera272@gmail.com>
2024-05-22 20:13:53 -0300

    Create index.c

---
+++ 00_IntroC/class0004/index.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int valor1;   // uma variável para um número inteiro chamada valor1
+    float valor2; // uma variável para um número real chamada valor2
+    char valor3;  // uma variável para uma letra chamada valor3
+
+    printf("Digite sua idade: ");
+    scanf("%d", &valor1);
+
+    printf("%d", valor1);
+
+    return 0;
+}
