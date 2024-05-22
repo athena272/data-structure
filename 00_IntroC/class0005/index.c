c8f436c9b22bf27805e39add5931be2ab8ab8613
athena272 <guilhermera272@gmail.com>
2024-05-22 20:16:28 -0300

    Create index.c

---
+++ 00_IntroC/class0005/index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float dado; // uma variável para um número real chamada dado
+    printf("Digite um valor real: ");
+    scanf("%f", &dado);
+
+    printf("%f", dado);
+
+    return 0;
+}
