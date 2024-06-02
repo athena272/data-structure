21bf025b26dfc569f8cc24e02419f68b64458410
athena272 <guilhermera272@gmail.com>
2024-06-01 23:23:35 -0300

    Create index.c

---
+++ 04_VetorMatriz/class0104/index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    int i;
+    int num2[40];
+
+    srand(time(NULL));
+
+    for(i = 0; i < 40; i++)
+        num2[i] = 1 + rand() % 99;
+
+    printf("\n\n");
+    for(i = 0; i < 40; i++)
+        printf("%d ", num2[i]);
+    printf("\n\n");
+
+    return 0;
+}
