b617675d80e7f4102726f6a94433d97c41469604
athena272 <guilhermera272@gmail.com>
2024-06-04 22:46:52 -0300

    Create index.c

---
+++ 04_VetorMatriz/class0121/index.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int tam = 5;
+    int l, c, mat[tam][tam];
+
+    srand(time(NULL));
+
+    for (l = 0; l < tam; l++)
+    {
+        for (c = 0; c < tam; c++)
+        {
+            mat[l][c] = rand() % 100;
+        }
+    }
+
+    for (l = 0; l < tam; l++)
+    {
+        for (c = 0; c < tam; c++)
+        {
+            printf("%d ", mat[l][c]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+    for (l = 0; l < tam; l++)
+    {
+        printf("%d ", mat[l][l]); // diagonal principal onde linha e coluna são iguais
+    }
+
+    return 0;
+}
