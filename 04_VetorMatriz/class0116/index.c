6e8e60d832f25d5552061ee7ea6780bcaa8988be
athena272 <guilhermera272@gmail.com>
2024-06-04 22:32:28 -0300

    Create index.c

---
+++ 04_VetorMatriz/class0116/index.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int i, opcao;
+    float vetor[10];
+
+    for(i = 0; i < 10; i++){
+        printf("Digite o valor da posicao %d: ", i);
+        scanf("%f", &vetor[i]);
+    }
+
+    do{
+        printf("\n0 - Fizalizar\n1 - Imprimir vetor\n2 - Imp. vetor invertido\n");
+        scanf("%d", &opcao);
+
+        switch(opcao){
+        case 0:
+            printf("Finalizando...\n");
+            break;
+        case 1:
+            for(i = 0; i < 10; i++){
+                printf("%.2f ", vetor[i]);
+            }
+            printf("\n");
+            break;
+        case 2:
+            for(i = 9; i >= 0; i--){
+                printf("%.2f ", vetor[i]);
+            }
+            printf("\n");
+            break;
+        default:
+            printf("Opcao invaalida!\n");
+        }
+
+    }while(opcao != 0);
+
+    return 0;
+}
