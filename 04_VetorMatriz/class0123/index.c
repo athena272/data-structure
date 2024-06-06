91578fd730692d05491d256b00761ff403b87f82
athena272 <guilhermera272@gmail.com>
2024-06-05 22:58:25 -0300

    Create index.c

---
+++ 04_VetorMatriz/class0123/index.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int tamL = 5, tamC = 4;
+    int l, c, mat[5][4], trans[4][5];
+
+    srand(time(NULL));
+
+    for (l = 0; l < tamL; l++)
+    {
+        for (c = 0; c < tamC; c++)
+        {
+            mat[l][c] = rand() % 500;
+        }
+    }
+
+    printf("\nMatriz original:\n");
+    for (l = 0; l < tamL; l++)
+    {
+        for (c = 0; c < tamC; c++)
+        {
+            printf("%3d ", mat[l][c]);
+        }
+        printf("\n");
+    }
+
+    for (l = 0; l < tamL; l++)
+    {
+        for (c = 0; c < tamC; c++)
+        {
+            trans[c][l] = mat[l][c];
+        }
+    }
+
+    printf("\nMatriz transposta:\n");
+    for (l = 0; l < tamC; l++)
+    {
+        for (c = 0; c < tamL; c++)
+        {
+            printf("%3d ", trans[l][c]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
