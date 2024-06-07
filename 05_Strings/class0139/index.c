3328732d8048eced510fdb7fa2a6c119fe2e0fad
athena272 <guilhermera272@gmail.com>
2024-06-06 23:56:18 -0300

    Create index.c

---
+++ 05_Strings/class0139/index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+
+    char palavra[50] = {"Bom dia simpatia!"};
+    char str[] = {"dia"};
+    char *ponteiro;
+
+    // procura pela string str dentro da string palavra
+    ponteiro = strstr(palavra, str);
+
+    // se ponteiro for diferente de null, imprime 3 caracteres (dia)
+    if (ponteiro)
+        printf("\n%c%c%c\n", *ponteiro, *(ponteiro + 1), *(ponteiro + 2));
+    else
+        printf("\nPonteiro nulo.\n");
+    return 0;
+}
