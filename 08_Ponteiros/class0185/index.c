012b7e38eb91f1b62dc89d5f5db0c0a1debe16d6
athena272 <guilhermera272@gmail.com>
2024-06-13 08:30:34 -0300

    Create index.c

---
+++ 08_Ponteiros/class0185/index.c
@@ -0,0 +1,21 @@
+/*
+
+          Aula 185: De quanta memória um ponteiro precisa?
+
+          Código escrito por Wagner Gaspar
+          Maior de 2021
+*/
+
+int main()
+{
+
+    int *ponteiro1;
+    char *ponteiro2;
+    float *ponteiro3;
+
+    printf("Tamanho: %d\n", sizeof(ponteiro1));
+    printf("Tamanho: %d\n", sizeof(ponteiro2));
+    printf("Tamanho: %d\n", sizeof(ponteiro3));
+
+    return 0;
+}
