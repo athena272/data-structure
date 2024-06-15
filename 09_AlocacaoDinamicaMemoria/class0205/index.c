1960ec795638ce8da32367485a0205d9816cbc23
athena272 <guilhermera272@gmail.com>
2024-06-14 23:56:23 -0300

    Create index.c

---
+++ 09_AlocacaoDinamicaMemoria/class0205/index.c
@@ -0,0 +1,53 @@
+/*
+            Aula 205: Dúvida
+
+            Código escrito por Wagner Gaspar
+            Junho de 2021
+
+        Dúvida: Faça uma função que recebe um nome e devolve o nome invertido.
+*/
+
+void inverter(char vet[])
+{
+    int i, aux, fim, tam = strlen(vet);
+
+    fim = tam - 1;
+    for (i = 0; i < tam / 2; i++)
+    {
+        aux = vet[i];
+        vet[i] = vet[fim];
+        vet[fim] = aux;
+        fim--;
+    }
+}
+
+char *inverter2(char vet[])
+{
+    int i, aux, fim, tam = strlen(vet);
+    char *novo = malloc((tam + 1) * sizeof(char));
+
+    fim = tam - 1;
+    for (i = 0; i < tam; i++)
+    {
+        novo[fim] = vet[i];
+        fim--;
+    }
+    novo[i] = '\0';
+    return novo;
+}
+
+int main()
+{
+
+    char palavra[25] = {"Muito bem feito!"};
+    char *p;
+
+    printf("%s\n", palavra);
+    // inverter(palavra);
+    printf("%s\n", palavra);
+
+    p = inverter2(palavra);
+    printf("%s\n", p);
+
+    return 0;
+}
